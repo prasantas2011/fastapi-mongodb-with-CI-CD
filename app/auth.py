fcbbfdd70e396374b543ad29c5f624a51827c8d0
prasantas2011 <prasantakus@gmail.com>
2025-09-10 17:26:30 +0530

    auth lining issue fixed

--- app/auth.py
+++ app/auth.py
@@ -1,10 +1,17 @@
+"""
+Authentication module for FastAPI project.
+
+Contains functions for login, JWT token creation, password hashing, and user authentication.
+"""
+
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from app.database import db
 from bson import ObjectId
+from app.database import db
+
 
 SECRET_KEY = "mysecret"  # 🔴 change in prod
 ALGORITHM = "HS256"
@@ -13,25 +20,31 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="users/login")
 
+
 def verify_password(plain, hashed):
+    """verify password."""
     return pwd_context.verify(plain, hashed)
 
 def hash_password(password):
+    """verify hash password."""
     return pwd_context.hash(password)
 
 async def authenticate_user(email: str, password: str):
+    """verify authenticate_user."""
     user = await db["users"].find_one({"email": email})
     if not user or not verify_password(password, user["password"]):
         return False
     return user
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    """create create_access_token."""
     to_encode = data.copy()
     expire = datetime.utcnow() + (expires_delta or timedelta(minutes=15))
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
+    """get get_current_user."""
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Invalid authentication",
@@ -42,9 +55,12 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         user_id = payload.get("sub")
         if user_id is None:
             raise credentials_exception
-    except JWTError:
-        raise credentials_exception
+    except JWTError as exc:
+        # Link the original JWTError to the new exception
+        raise credentials_exception from exc
+
     user = await db["users"].find_one({"_id": ObjectId(user_id)})
     if user is None:
         raise credentials_exception
+
     return user
